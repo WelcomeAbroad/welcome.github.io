ea4974ad315769bab3f3585945e4c306db296b4d
WelcomeAbroad <welcomeabroad016@gmail.com>
2020-09-29 04:01:26 +0300

    php update

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -1,69 +1,28 @@
 ﻿<?php
-
-// Replace this with your own email address
-$siteOwnersEmail = 'user@website.com';
-
-
-if($_POST) {
-
-    $name = trim(stripslashes($_POST['contactName']));
-    $email = trim(stripslashes($_POST['contactEmail']));
-    $subject = trim(stripslashes($_POST['contactSubject']));
-    $contact_message = trim(stripslashes($_POST['contactMessage']));
-
-    // Check Name
-    if (strlen($name) < 2) {
-        $error['name'] = "Please enter your name.";
-    }
-    // Check Email
-    if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
-        $error['email'] = "Please enter a valid email address.";
-    }
-    // Check Message
-    if (strlen($contact_message) < 15) {
-        $error['message'] = "Please enter your message. It should have at least 15 characters.";
-    }
-    // Subject
-    if ($subject == '') { $subject = "Contact Form Submission"; }
+// несколько получателей
+$to  = 'welcomeabroad016@gmail.com' . ', ';  // обратите внимание на запятую
 
 
-    // Set Message
-    $message .= "Email from: " . $name . "<br />";
-    $message .= "Email address: " . $email . "<br />";
-    $message .= "Message: <br />";
-    $message .= $contact_message;
-    $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+// тема письма
+$subject = 'Заявка';
 
-    // Set From: header
-    $from =  $name . " <" . $email . ">";
+// текст письма
+$message = $_POST['contactName']
+    . '<br />'
+    . $_POST['contactPhone'] . '<br />'
+    . $_POST['contactAge']
+    . '<br />' . $_POST['contactStudy']
+    . '<br />' . $_POST['contactNationality']
+    . '<br />' . $_POST['contactLanguage'];
 
-    // Email Headers
-    $headers = "From: " . $from . "\r\n";
-    $headers .= "Reply-To: ". $email . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+// Для отправки HTML-письма должен быть установлен заголовок Content-type
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Дополнительные заголовки
+$headers .= 'To: Ruslan <rodionov.off@gmail.com>' . "\r\n"; // Свое имя и email
+$headers .= 'From: '  . $_POST['contactName'] . '<' . $_POST['contactPhone'] . '>' . "\r\n";
 
 
-    if (!$error) {
-
-        ini_set("sendmail_from", $siteOwnersEmail); // for windows server
-        $mail = mail($siteOwnersEmail, $subject, $message, $headers);
-
-        if ($mail) { echo "OK"; }
-        else { echo "Something went wrong. Please try again."; }
-        
-    } # end if - no validation error
-
-    else {
-
-        $response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
-        $response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
-        $response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-        
-        echo $response;
-
-    } # end if - there was a validation error
-
-}
-
-?>
+// Отправляем
+mail($to, $subject, $message, $headers);
